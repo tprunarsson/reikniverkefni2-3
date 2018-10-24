e10a5a00af6ac70b22b1e144f368b970bbd118ac
sigrundish <sigrun@dohop.com>
2018-10-24 20:25:34 +0000

    dyna2 file

---
+++ dyna2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Dyna-2 using the function approximator architecture developed in part 1. Illustrate
+the performance of this player against the random player during learning.
+Compare the performance of this player against the neural network from part
+1.
+"""
+
